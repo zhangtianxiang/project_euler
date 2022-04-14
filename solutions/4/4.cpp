2b98ec1418baf93ec187b6b239b7f50b5defd75c
tysenzhang <tysenzhang@tencent.com>
2022-04-14 21:48:54 +0800

    feat: solution 4

--- solutions/4/4.cpp
+++ solutions/4/4.cpp
@@ -1,8 +1,38 @@
 #include <cstdio>
+#include <set>
+
+std::set<int> palindroms;
+
+char buf[16];
+
+void check(int x) {
+  int n = sprintf(buf, "%d", x);
+  for (int i = (n / 2) - 1; i >= 0; i--) {
+    if (buf[i] != buf[n - i - 1]) return;
+  }
+  palindroms.insert(x);
+}
+
+inline void pre() {
+  for (int i = 100; i <= 999; i++) {
+    for (int j = i; j <= 999; j++) {
+      check(i * j);
+    }
+  }
+}
+
+inline void work() {
+  int n;
+  scanf("%d", &n);
+  auto it = palindroms.lower_bound(n);
+  it--;
+  printf("%d\n", *it);
+}
 
 int main() {
-  int x;
-  scanf("%d", &x);
-  printf("%d\n", x);
+  pre();
+  int t;
+  scanf("%d", &t);
+  while (t-- > 0) work();
   return 0;
 }
